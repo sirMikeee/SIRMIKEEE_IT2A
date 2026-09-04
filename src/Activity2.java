8e52c165619188265c1cbb8608b4a8bd1cae8363
sirMikeee <mike@stcecilia.edu.ph>
2026-09-04 09:59:28 +0800

    activity 2

---
+++ src/Activity2.java
@@ -0,0 +1,8 @@
+public class Activity2 {
+
+    public void test(){
+
+        System.out.println("Hello from Activity2");
+    }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ public static void main(String[] args){
     Scanner sc = new Scanner(System.in);
     System.out.println("----- LIST OF ACTIVITIES -----");
     System.out.println("1. Activity 1");
+    System.out.println("1. Activity 2");
     System.out.println("------------------------------");
     System.out.print("Enter selection: ");
     int select = sc.nextInt();
@@ -12,6 +13,9 @@ public static void main(String[] args){
     if(select == 1){
         Activity1 act1 = new Activity1();
         act1.greeting();
+    }else if(select == 2){
+        Activity2 act2 = new Activity2();
+        act2.test();
     }else{
 
         System.out.println("Invalid Selection");
